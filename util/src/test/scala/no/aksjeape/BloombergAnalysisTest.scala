baa1e4157bd19267ad27959b013b139634157876
Kazi Abdullah Saikat <kazi.saikat@bouvet.no>
2014-06-13 15:36:24 +0200

    Generate csv file with aksje information

--- util/src/test/scala/no/aksjeape/BloombergAnalysisTest.scala
+++ util/src/test/scala/no/aksjeape/BloombergAnalysisTest.scala
@@ -1,6 +1,8 @@
 package no.aksjeape
 
-import org.scalatest.{FunSuite, FunSpec}
+import java.io.{File, PrintWriter, FileWriter}
+
+import org.scalatest.FunSuite
 import org.htmlcleaner.{TagNode, CleanerProperties, HtmlCleaner}
 import scalaj.http.Http.Request
 import scalaj.http.{HttpOptions, Http}
@@ -10,11 +12,13 @@ class BloombergAnalysisTest extends FunSuite {
 
     val connTimeout = 3000
     val responseTimeout = 10000
-    val companiesUrl = "http://www.bloomberg.com/markets/companies/country/sweden/"
+    val companiesUrl = "http://www.bloomberg.com/markets/companies/country/norway/"
 
 
     def pageContent(url:String):String = {
-        val urlReq: Request = Http(url).option(HttpOptions.connTimeout(connTimeout)).option(HttpOptions.readTimeout(responseTimeout))
+        val urlReq: Request = Http(url)
+            .option(HttpOptions.connTimeout(connTimeout))
+            .option(HttpOptions.readTimeout(responseTimeout))
         assert(urlReq.responseCode == 200, () => println("url: " + urlReq))
         urlReq.asString
     }
@@ -72,34 +76,52 @@ class BloombergAnalysisTest extends FunSuite {
         companySymbolList.map(value => new URI(companiesUrl).resolve(value._1.getAttributeByName("href")).toString)
     }
 
-    def companyPERatio(companyUrl:String):Double = {
-
-        println("Retrieving company information for '" + companyUrl + "'")
-
+    def companyPERatioOgDividend(companyUrl:String):Option[(Double, String, String)] = {
         val htmlCleaner = createHtmlCleaner
         val companyPageNode: TagNode = htmlCleaner.clean(pageContent(companyUrl))
 
         val keyStatTableList: Array[TagNode] = companyPageNode.getElementsByAttValue("class", "key_stat_data", true, false)
         if (keyStatTableList.isEmpty) {
-            return -1.0
+            return Option.empty
         }
-        val keyStatTable: TagNode = keyStatTableList(0).getElementsByName("tbody", false)(0)
-        assert(keyStatTable != null)
+        val keyStatTableOpt: Option[TagNode] = keyStatTable(companyPageNode)
 
-        val statRows: Array[TagNode] = keyStatTable.getElementsByName("tr", false)
+        val companyStats:Array[(String, String)] = keyStatTableOpt match {
+            case Some(table) => statistics(table)
+            case None => Array[(String, String)]()
+        }
+
+        val peRatio: Double = statisticsValue(companyStats, "Current P/E Ratio") match {
+            case Some(ratio) => ratio.toDouble
+            case None => -1
+        }
+        val cashDividend: String = statisticsValue(companyStats, "Cash Dividend") match {
+            case Some(dividend) => dividend
+            case None => ""
+        }
+        val estimatedEps: String = statisticsValue(companyStats, "Est. EPS") match {
+            case Some(eps) => eps
+            case None => ""
+        }
+        Some((peRatio, cashDividend, estimatedEps))
+    }
+
+    def statisticsValue(values:Array[(String, String)], name:String):Option[String] = {
+        values.find(value => value._1.startsWith(name)).map(_._2)
+    }
+
+    def statistics(statisticsTable:TagNode):Array[(String, String)] = {
+        val statRows: Array[TagNode] = statisticsTable.getElementsByName("tr", false)
         assert(!statRows.isEmpty)
 
-        val companyStats = statRows.map(node => (node.findElementByName("th", false).getText.toString, node.findElementByAttValue("class", "company_stat", false, false).getText.toString))
+        val companyStats = statRows.map(node =>
+            (
+                node.findElementByName("th", false).getText.toString,
+                node.findElementByAttValue("class", "company_stat", false, false).getText.toString
+            )
+        )
         assert(!companyStats.isEmpty)
-
-        val peRatioList = companyStats.filter(value => "Current P/E Ratio (ttm)".equalsIgnoreCase(value._1))
-        val peRatio = peRatioList.map(value => value._2).head
-        assert(peRatio != null)
-        try {
-            peRatio.toDouble
-        } catch {
-            case _ => -1.0
-        }
+        return companyStats
     }
 
     test("Dumps PE-ratio from bloomberg") {
@@ -109,10 +131,31 @@ class BloombergAnalysisTest extends FunSuite {
         val companyLinks: Array[String] = urlToCompaniesPages.map(value => companyList(value)).flatten
         assert(!companyLinks.isEmpty)
 
-        val peSortedCompanies: Array[(String, Double)] = companyLinks.map(value => (value, companyPERatio(value))).sortWith(_._2 < _._2)
-        peSortedCompanies.foreach(value => println(value._1 + " = " + value._2))
+        // val companyLinks = Array("http://www.bloomberg.com/quote/NIS:SS")
+        val infoCsv = new PrintWriter(new FileWriter(new File(java.lang.System.getProperty("user.home"), "aksjeinfo.csv")))
+        infoCsv.println("Url, P/E Ratio, Cash Dividend, Estimated EPS")
+
+        companyLinks.foreach(companyLink =>  {
+            companyPERatioOgDividend(companyLink) match {
+                case Some((peRatio, cashDividend, estimatedEps)) => {
+                    if (cashDividend != "-" && estimatedEps != "-") {
+                        infoCsv.println(java.lang.String.format("%s,%s,%s,%s", companyLink, peRatio.toString, cashDividend, estimatedEps))
+                    }
+                }
+            }
+        })
+        infoCsv.close()
     }
 
+    def keyStatTable(companyPageNode:TagNode):Option[TagNode] = {
+        val keyStatTableList: Array[TagNode] = companyPageNode.getElementsByAttValue("class", "key_stat_data", true, false)
+        if (keyStatTableList.isEmpty) {
+            return Option.empty
+        }
+        val keyStatTable: TagNode = keyStatTableList(0).getElementsByName("tbody", false)(0)
+        assert(keyStatTable != null)
+        Some(keyStatTable)
+    }
 
     def createHtmlCleaner : HtmlCleaner = {
         val htmlCleaner = new HtmlCleaner
